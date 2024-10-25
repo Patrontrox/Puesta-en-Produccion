a97b23b34b11d50a1a6177bb83f1a96b875f65c6
Patrontrox <sgn_juanlu@protonmail.com>
2024-10-25 18:57:02 +0200

    Delete Ejercicio2.sh

--- Ejercicio2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Verifica si el segundo parámetro es un archivo existente y lo muestra
-if [ -e "$2" ]; then
-    cat "$2"
-    echo "Código de salida: $?"
-else
-    echo "No se puede acceder al archivo o no existe."
-    echo "Código de salida: $?"
-fi
